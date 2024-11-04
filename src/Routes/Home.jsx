d761e4c240dd835977b0d0da4b2f2f7ebd4f132d
fbelam <fabricio.novaes@hotmail.com>
2024-11-03 23:50:13 -0300

    inicio da API

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+import "./Home.css";
 
 const Home = () => {
+
+  const [posts, setposts] = useState([])
+
+
+  const getPosts = async () => {
+
+    try {
+      const response = await axios.get("'https://jsonplaceholder.typicode.com/posts'")
+
+      const data = response.data;
+      setposts(data);
+
+    } catch (error) {
+      console.log(error);
+    }
+
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+
   return (
-    <div>Home</div>
+    <div>
+      <h1>Últimos Posts</h1>
+      {posts.length === 0 ? <p>Carregando...</p> : (
+        
+      )}
+    </div>
+
   )
 }
 
