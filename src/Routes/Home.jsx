f3766666406f5610bb717a52bc9fb17ffe49fd4e
fbelam <fabricio.novaes@hotmail.com>
2024-11-04 00:52:09 -0300

    ajusta do css do ultimos posts

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -12,10 +12,11 @@ const Home = () => {
   const getPosts = async () => {
 
     try {
-      const response = await axios.get("'https://jsonplaceholder.typicode.com/posts'")
+      const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
 
       const data = response.data;
       setposts(data);
+      console.log(data);
 
     } catch (error) {
       console.log(error);
@@ -32,7 +33,13 @@ const Home = () => {
     <div>
       <h1>Últimos Posts</h1>
       {posts.length === 0 ? <p>Carregando...</p> : (
-        
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/post/${post.id}`} className='btn'>Leia mais</Link>
+          </div>
+        ))
       )}
     </div>
 
